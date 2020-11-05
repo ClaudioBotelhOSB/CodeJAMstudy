3eacaf85d51ce5c46b33d9a8c4f9b8e8b1289588
Claudio Botelho <43050335+ClaudioBotelhOSB@users.noreply.github.com>
2020-11-05 08:26:33 -0300

    Update warmup_A.cpp

--- ICPC/2019/South_America-Brazil-First_Phase/warmup_A.cpp
+++ ICPC/2019/South_America-Brazil-First_Phase/warmup_A.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
-int main(){
+int main() {
+  int c = 0, //capacidade
+    a = 0, //alunos
+    v = 0; //viagens
 
-
-    return 0;
+  cin>>c>>a;
+  c=c-1; // tirar monitor
+  v = (a%c == 0)?(a/c):(a/c)+1;
+  cout<<v;
 }
